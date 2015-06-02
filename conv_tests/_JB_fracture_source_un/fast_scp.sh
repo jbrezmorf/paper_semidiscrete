8622c88610efcf2c61f48bd4ad1ec94d041d366f
Jan Brezina <jan.brezina@tul.cz>
2015-06-02 15:08:46 +0200

    Update aux scripts.

--- conv_tests/_JB_fracture_source_un/fast_scp.sh
+++ conv_tests/_JB_fracture_source_un/fast_scp.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-rsync -ravz --update --exclude='*/core' jan_brezina@tarkil.cesnet.cz:/auto/praha1/jan_brezina/clanek/conv_tests/_JB_exp_sin/$1 .
+subdir=`pwd`
+subdir_base=${subdir##*/}
+time rsync -ravz --update --exclude='*/core' jan_brezina@tarkil.cesnet.cz:/auto/praha1/jan_brezina/clanek/conv_tests/${subdir_base}/$1 .
